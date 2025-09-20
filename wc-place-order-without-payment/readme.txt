471975cc47927677cced831fca49154a39e084b4
Sean Murphy <sean@iamseanmurphy.com>
2025-09-20 06:19:18 +0000

    2.7.3

--- wc-place-order-without-payment/readme.txt
+++ wc-place-order-without-payment/readme.txt
@@ -4,8 +4,8 @@ Donate link: https://nitin247.com/buy-me-a-coffee
 Tags: pre order, woocommerce pre order, request quote, quote request, quote plugin, 
 Requires at least: 6.2
 Tested up to: 6.8
-Stable tag: 2.7.2
-Version: 2.7.2
+Stable tag: 2.7.3
+Version: 2.7.3
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -141,8 +141,10 @@ For support or any inquiries, please contact us at <a href="mailto:nitin247@outl
 Enjoy using the Place order without payment plugin!
 
 == Changelog ==
+** V 2.7.3 **
+* SDK Update
 ** V 2.7.2 **
-* SDK Update.
+* SDK Update
 ** V 2.7.1 **
 * POT Update + Bug fixes.
 ** V 2.7.0 **
@@ -207,5 +209,5 @@ Stable initial release.
 
 == Upgrade Notice ==
 
-= 2.7.2 = 
-* SDK Update & Code Optimizations.
+= 2.7.3 = 
+* SDK Update
--- wc-place-order-without-payment/vendor/composer/installed.php
+++ wc-place-order-without-payment/vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'neebplugins/wc-place-order-without-payment',
-        'pretty_version' => '2.6.6',
-        'version' => '2.6.6.0',
+        'pretty_version' => '2.7.3',
+        'version' => '2.7.3.0',
         'reference' => null,
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -11,17 +11,17 @@
     ),
     'versions' => array(
         'freemius/wordpress-sdk' => array(
-            'pretty_version' => '2.12.1',
-            'version' => '2.12.1.0',
-            'reference' => 'fedeba08eb3e62af5f84bd6d602f98636467cffc',
+            'pretty_version' => '2.12.2',
+            'version' => '2.12.2.0',
+            'reference' => '241fbfc91151f85d8ebeb75343caf29bda1d3208',
             'type' => 'library',
             'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'neebplugins/wc-place-order-without-payment' => array(
-            'pretty_version' => '2.6.6',
-            'version' => '2.6.6.0',
+            'pretty_version' => '2.7.3',
+            'version' => '2.7.3.0',
             'reference' => null,
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
